dbf584952eccd015deab284216ebc023ae4b4b3d
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-09 10:11:02 +0000

    First Commit

---
+++ Testing Websites using Selenuim/Main.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        IWebDriver element = new ChromeDriver(); 
+        }
+    }
